98e03c7a82f17eb31b023158f671ea5c2b06db72
Summon Yang <SummonYang1991@gmail.com>
2015-10-05 21:46:20 +0800

    test stl member template

---
+++ chapter01/test_member.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class alloc {
+
+};
+
+template <typename T, typename Alloc = alloc>
+class vector {
+public:
+    typedef T value_type;
+    typedef value_type* iterator;
+    
+    template <typename I>
+        void insert(iterator position, I first, I last) {
+            cout << "insert()" << endl;
+        }
+};
+
+int main()
+{
+    int ia[5] = {0, 1, 2, 3, 4};
+    vector<int> x;
+    vector<int>::iterator ite;
+    x.insert(ite, ia, ia + 5);
+    return 0;
+}
